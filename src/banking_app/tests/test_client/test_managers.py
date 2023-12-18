705b9eebb58538f168023527778e4cec5e540f92
Deyvidas <devidbanyavichyus@yandex.ru>
2023-12-18 17:00:32 +0400

    Added tests for method update of the ClientManager.

--- src/banking_app/tests/test_client/test_managers.py
+++ src/banking_app/tests/test_client/test_managers.py
@@ -1,6 +1,8 @@
 import pytest
 
+from copy import deepcopy
 from random import choice
+from pydantic import TypeAdapter
 
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.sql.expression import select
@@ -11,6 +13,7 @@ from sqlalchemy.orm.session import Session
 from src.banking_app.models.client import Client
 from src.banking_app.schemas.client import BaseClientModel
 from src.banking_app.tests.test_client.conftest import BaseTestClient
+from src.banking_app.tests.test_client.factory import ClientFactory
 
 
 @pytest.mark.run(order=1.01_00)
@@ -21,7 +24,7 @@ class TestCreate(BaseTestClient):
             session: Session,
             clients_dto: Sequence[BaseClientModel],
     ):
-        client_data = self.get_required_values(choice(clients_dto))
+        client_data = self.get_orm_data_from_dto(choice(clients_dto))
 
         # Test that create returns the created instance.
         statement = self.manager.create(**client_data)
@@ -44,7 +47,7 @@ class TestCreate(BaseTestClient):
             session: Session,
             clients_dto: Sequence[BaseClientModel],
     ):
-        client_data = self.get_required_values(choice(clients_dto))
+        client_data = self.get_orm_data_from_dto(choice(clients_dto))
         # Remove all field which have default value in ORM model.
         [client_data.pop(field) for field in self.default_values.keys()]
 
@@ -70,7 +73,7 @@ class TestCreate(BaseTestClient):
             session: Session,
             clients_dto: Sequence[BaseClientModel]
     ):
-        client_data = self.get_required_values(choice(clients_dto))
+        client_data = self.get_orm_data_from_dto(choice(clients_dto))
 
         # Create instance of status in DB.
         statement = self.manager.create(**client_data)
@@ -102,7 +105,7 @@ class TestBulkCreate(BaseTestClient):
             session: Session,
             clients_dto: Sequence[BaseClientModel],
     ):
-        list_kwargs = [self.get_required_values(c) for c in clients_dto]
+        list_kwargs = [self.get_orm_data_from_dto(c) for c in clients_dto]
 
         # Test if bulk_create returns a list of created instances.
         statement = self.manager.bulk_create(list_kwargs)
@@ -119,7 +122,7 @@ class TestBulkCreate(BaseTestClient):
             session: Session,
             clients_dto: Sequence[BaseClientModel],
     ):
-        list_kwargs = [self.get_required_values(c) for c in clients_dto]
+        list_kwargs = [self.get_orm_data_from_dto(c) for c in clients_dto]
         # Remove all field which have default value in ORM model.
         for field in self.default_values.keys():
             [k.pop(field) for k in list_kwargs]
@@ -138,13 +141,13 @@ class TestBulkCreate(BaseTestClient):
         half = int(len(clients_dto) / 2)
 
         # Saving the first part of having clients.
-        first_half = [self.get_required_values(c) for c in clients_dto[:half]]
+        first_half = [self.get_orm_data_from_dto(c) for c in clients_dto[:half]]
         statement = self.manager.bulk_create(first_half)
         instances = session.scalars(statement).unique().all()
         session.commit()
 
         # Try to create new clients that are mixed with already existing clients.
-        second_half = [self.get_required_values(c) for c in clients_dto[half:]]
+        second_half = [self.get_orm_data_from_dto(c) for c in clients_dto[half:]]
         second_half += [first_half[0], first_half[-1]]
         statement = self.manager.bulk_create(second_half)
         with pytest.raises(IntegrityError) as error:
@@ -194,10 +197,73 @@ class TestFilter(BaseTestClient):
 
 @pytest.mark.run(order=1.01_03)
 class TestUpdate(BaseTestClient):
+    AdapterOne = TypeAdapter(BaseTestClient.model_dto).validate_python
 
     def test_update_single_client_with_client_id(
             self,
             session: Session,
             clients_orm: Sequence[Client],
     ):
-        ...
+        client_to_update = choice(clients_orm)
+
+        # Prepare data.
+        new_client_dto = ClientFactory().build(factory_use_construct=True)
+        while self.AdapterOne(client_to_update) == new_client_dto:
+            client_to_update = choice(clients_orm)
+        client_to_update = deepcopy(client_to_update)  # To save original state.
+        new_data = self.get_orm_data_from_dto(new_client_dto, exclude={'client_id'})
+
+        # Update must change single client and return it with updated field values.
+        statement = self.manager.update(
+            where=dict(client_id=client_to_update.client_id),
+            set_value=new_data,
+        )
+        instance = session.scalars(statement).unique().all()
+        session.commit()
+        assert len(instance) == 1
+        assert isinstance(instance := instance[0], self.model_orm)
+
+        # Check that the updated client has been returned with updated fields.
+        new_data['client_id'] = client_to_update.client_id
+        self.compare_obj_before_after(new_data, instance)
+
+        # Ensure that the object has new values in the DB.
+        statement = self.manager.filter(client_id=instance.client_id)
+        instance_after = session.scalars(statement).unique().all()
+        assert len(instance_after) == 1
+        assert isinstance(instance_after := instance_after[0], self.model_orm)
+        self.compare_obj_before_after(instance, instance_after)
+
+    def test_update_unexistent_status(
+            self,
+            session: Session,
+            clients_orm: Sequence[Client],
+    ):
+        instances_before = deepcopy(clients_orm)
+
+        # Prepare new data.
+        new_data = ClientFactory.build(factory_use_construct=True)
+        new_data = self.get_orm_data_from_dto(new_data, exclude={'client_id'})
+
+        # Make an attempt to update the status with an unexistent status_num.
+        unexist_client_id = self.get_unexistent_client_id(clients_orm)
+        statement = self.manager.update(
+            where=dict(client_id=unexist_client_id),
+            set_value=new_data,
+        )
+        instance = session.scalars(statement).unique().all()
+        session.commit()
+        assert len(instance) == 0
+
+        # Ensure that the objects in the DB not been changed.
+        statement = self.manager.filter()
+        instances_after = session.scalars(statement).unique().all()
+        self.compare_list_before_after(instances_before, instances_after)
+
+    def test_update_without_new_values(self, clients_orm: Sequence[Client]):
+        # Make an attempt to create statement without values.
+        client_id = choice(clients_orm).client_id
+        with pytest.raises(ValueError) as error:
+            self.manager.update(where=dict(client_id=client_id), set_value=dict())
+        msg = 'Without new values, updating can\'t proceed, set_value={}.'
+        assert str(error.value) == msg
